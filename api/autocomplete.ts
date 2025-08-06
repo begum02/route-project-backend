080d70fab3d7e0fadb1d0af75f2214c5bf4efce2
begum02 <aysebegumkoyuncu51@gmail.com>
2025-08-06 14:33:36 +0200

    headerlarda degisiklik

--- api/autocomplete.ts
+++ api/autocomplete.ts
@@ -7,7 +7,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization,Accept,X-Requested-With');
   res.setHeader('Set-Cookie', 'clear=; Max-Age=0');
 
   if(req.method=== 'OPTIONS'){
